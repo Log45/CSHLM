1e0ab1fcce9cec8f1afb696ba225bb860c435992
Log45 <Skyker604@gmail.com>
2023-09-25 14:59:19 -0400

    convert json to one txt file

--- dataset.py
+++ dataset.py
@@ -2,27 +2,29 @@ from torch.utils.data import DataLoader, IterableDataset
 import os
 import json
 
-def main():
-    data = []
+def totxt():
     text = []
     current = os.getcwd()
-    for dir in os.listdir(f"{current}/data"):
-        for file in os.listdir(f"{current}/data/{dir}"):
-            file = f"{current}/data/{dir}/{file}"
-            #print(f"{current}/data/{dir}/{file}")
-            data.append(file)
+    for dir in os.listdir(f"{current}/data/JSON"):
+        for file in os.listdir(f"{current}/data/JSON/{dir}"):
+            file = f"{current}/data/JSON/{dir}/{file}"
             with open(file, encoding="UTF-8") as f:
                 s = f.read().replace('\n', '')
                 d = json.loads(s)
                 #print(d)
                 for x in d:
                     for i in x:
-                        #print((x))
                         if type(x[i]) is dict:
                             if 'text' in x[i].keys():
                                 t = x[i]['text']
                                 text.append(t)
-                        #print((x[i]))
     print(text)
+    messages = ""
+    for s in text:
+        messages = f"{messages}{s}\n"
 
-main()
+    with open("data/messages.txt", encoding="UTF-8") as f:
+        f.write(messages)
+
+if __name__ == "__main__":
+    totxt()
