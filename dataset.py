bc57e5429e13e0a3e4654b4233d4999eaede71da
Log45 <Skyker604@gmail.com>
2023-09-25 14:43:12 -0400

    simple deletions

--- dataset.py
+++ dataset.py
@@ -1,19 +1,6 @@
 from torch.utils.data import DataLoader, IterableDataset
 import os
-import bisect
 import json
-
-class JsonDataset(IterableDataset):
-    def __init__(self, files):
-        self.files = files
-
-    def __iter__(self):
-        for json_file in self.files:
-            with open(json_file) as f:
-                for sample_line in f:
-                    sample = json.loads(sample_line)
-                    print(sample['text'])
-                    yield sample['text']
 
 def main():
     data = []
@@ -37,9 +24,5 @@ def main():
                                 text.append(t)
                         #print((x[i]))
     print(text)
-
-    #dataset = JsonDataset(data)
-    #dataloader = DataLoader(dataset, batch_size=32)
-    #print(dataset, "\n", dataloader)
 
 main()
